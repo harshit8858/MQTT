6bd78b61448abdd225aebe1ed17f94aa0012905b
harshit verma <harshit8858@gmail.com>
2020-03-20 00:24:10 +0530

    Api ready - tested!

--- authentication/views.py
+++ authentication/views.py
@@ -5,12 +5,5 @@ from utils.utils import request_type, jsontype
 
 
 class LoginApi(APIView):
-    """
-    Login/signup api
-    Parameters:mobile
-    Returns:Otp
-    """
-
     def post(self, request):
-        data = request_type(request)
         return Response({'res': 1, 'msg': 'Done!'})
