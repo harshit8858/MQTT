cb1b0250d8e4a676bed4918a9e2c476ba8245c2b
harshit verma <harshit8858@gmail.com>
2020-03-22 21:18:04 +0530

    csrf_exempt in mqtt

--- mqtt/views.py
+++ mqtt/views.py
@@ -2,12 +2,14 @@ import paho.mqtt.client as mqtt
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from utils.utils import request_type, jsontype
+from django.views.decorators.csrf import csrf_exempt
 
 client = mqtt.Client("C1")  # Client 1
 client.connect("broker.hivemq.com")  # MQTT server
 
 
 class checkMQTTApi(APIView):
+    @csrf_exempt
     def post(self, request):
         try:
             data = request_type(request)
