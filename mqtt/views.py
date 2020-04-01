d427e62e21c20c7abbc742f8b2753b14b77a47e9
harshit verma <harshit8858@gmail.com>
2020-04-01 22:55:34 +0530

    Update

--- mqtt/views.py
+++ mqtt/views.py
@@ -1,11 +1,24 @@
 import paho.mqtt.client as mqtt
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from utils.utils import request_type, jsontype
-from django.views.decorators.csrf import csrf_exempt
+from utils.utils import request_type
+# from django.views.decorators.csrf import csrf_exempt
 
-client = mqtt.Client("C1")  # Client 1
-client.connect("broker.hivemq.com")  # MQTT server
+
+'''
+create client > connect client > client.loop_start() > client.is_connected() = True
+'''
+
+client = mqtt.Client("clientId-8858414441")  # Client 1
+def on_message(client, userdata, message):
+    print("message received " ,str(message.payload.decode("utf-8")))
+    print("message topic=",message.topic)
+    print("message qos=",message.qos)
+    print("message retain flag=",message.retain)
+
+client.on_message = on_message
+
+client.connect("broker.mqttdashboard.com")  # MQTT server
 
 
 class checkMQTTApi(APIView):
@@ -16,14 +29,18 @@ class checkMQTTApi(APIView):
             flag = int(data['flag'])
             print(data['flag'])
             # client.loop_start()  # start the loop
+            print("Subscribing to topic", "home_mqtt_topic/1")
+            client.loop_start()
             client.subscribe("home_mqtt_topic/1")
             if flag == 1:
                 client.publish("home_mqtt_topic/1", "ON")
+                print("Publishing message to topic", "home_mqtt_topic/1 - ON")
                 msg = 'ON'
             else:
                 client.publish("home_mqtt_topic/1", "OFF")
+                print("Publishing message to topic", "home_mqtt_topic/1 - OFF")
                 msg = 'OFF'
-            # client.loop_stop()  # stop the loop
+            client.loop_stop()  # stop the loop
             status = 1
         except Exception as e:
             print(e)
